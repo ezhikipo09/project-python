fa55657e9d83fd6ae3d7e81ac7802d1b6d27b17e
Ezhiksxl <jokeryt30@gmail.com>
2023-12-10 21:17:32 +0600

    Version 1.2

--- restaurant.sql
+++ restaurant.sql
@@ -1,91 +1,63 @@
-CREATE TABLE manager (
-    manager_id INT PRIMARY KEY,
-    name VARCHAR(255),
-    phone_number VARCHAR(12)
-);
-
-insert into manager (manager_id , name, phone_number) values (1, 'Yehudi', '864-678-3584');
-insert into manager (manager_id , name, phone_number) values (2, 'Jourdan', '204-931-2776');
-insert into manager (manager_id , name, phone_number) values (3, 'Shayne', '998-355-0426');
-insert into manager (manager_id , name, phone_number) values (4, 'Hallie', '477-628-2488');
-insert into manager (manager_id , name, phone_number) values (5, 'Fania', '284-419-3993');
-insert into manager (manager_id , name, phone_number) values (6, 'Karena', '377-512-1820');
-insert into manager (manager_id , name, phone_number) values (7, 'Annamaria', '132-871-0907');
-insert into manager (manager_id , name, phone_number) values (8, 'Elyssa', '140-481-5344');
-insert into manager (manager_id , name, phone_number) values (9, 'Creigh', '359-517-1610');
-insert into manager (manager_id , name, phone_number) values (10, 'Karlie', '299-655-9856');
-
-CREATE TABLE restaurant (
-    restaurant_id INT PRIMARY KEY,
-    name VARCHAR(255),
-    address VARCHAR(255),
-    manager_id INT REFERENCES manager(manager_id)
-);
-
-insert into restaurant (restaurant_id , name, address, manager_id) values (1, 'Flavorsome Fusion', '4 Vidon Drive', 1);
-insert into restaurant (restaurant_id , name, address, manager_id) values (2, 'Foodie''s Paradise', '87 Mockingbird Parkway', 2);
-insert into restaurant (restaurant_id , name, address, manager_id) values (3, 'Culinary Delights', '200 Stone Corner Crossing', 3);
-insert into restaurant (restaurant_id , name, address, manager_id) values (4, 'The Gourmet Grill', '5804 Di Loreto Crossing', 4);
-insert into restaurant (restaurant_id , name, address, manager_id) values (5, 'Taste of Heaven', '3476 Stang Lane', 5);
-insert into restaurant (restaurant_id , name, address, manager_id) values (6, 'Savory Bites', '46987 Londonderry Court', 6);
-insert into restaurant (restaurant_id , name, address, manager_id) values (7, 'The Cozy Cafe', '1817 Hintze Center', 7);
-insert into restaurant (restaurant_id , name, address, manager_id) values (8, 'Savory Bites', '5 Charing Cross Way', 8);
-insert into restaurant (restaurant_id , name, address, manager_id) values (9, 'The Hungry Moose', '76781 Elgar Plaza', 9);
-insert into restaurant (restaurant_id , name, address, manager_id) values (10, 'The Cozy Cafe', '8627 Hayes Circle', 10);
-
-CREATE TABLE bartender (
-    bartender_id INT PRIMARY KEY,
-    name VARCHAR(255),
-    phone_number VARCHAR(12),
-    restaurant_id INT REFERENCES restaurant(restaurant_id)
-);
-
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (1, 'Huntley', '108-486-8331', 1);
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (2, 'Clarice', '132-816-7137', 2);
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (3, 'Stanford', '371-722-8819', 3);
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (4, 'Suzy', '381-137-9613', 4);
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (5, 'Corella', '634-440-6859', 5);
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (6, 'Genovera', '347-232-3553', 6);
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (7, 'Adorne', '696-578-6417', 7);
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (8, 'Ransom', '231-707-9366', 8);
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (9, 'Cornelius', '727-721-6174', 9);
-insert into bartender (bartender_id, name, phone_number, restaurant_id) values (10, 'Liz', '400-122-1356', 10);
 
 CREATE TABLE tables (
     table_id INT PRIMARY KEY,
-    table_number INT,
-    is_reserved BOOLEAN,
-    restaurant_id INT REFERENCES restaurant(restaurant_id)
+	reserved_by text,
+    table_number INT ,
+    is_reserved BOOLEAN
 );
 
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (1, 1, true, 1);
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (2, 2, true, 2);
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (3, 3, false, 3);
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (4, 4, true, 4);
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (5, 5, false, 5);
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (6, 6, false, 6);
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (7, 7, false, 7);
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (8, 8, true, 8);
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (9, 9, true, 9);
-insert into tables (table_id, table_number, is_reserved, restaurant_id) values (10, 10, false, 10);
+insert into tables (table_id, table_number, is_reserved) values (1, 1, false);
+insert into tables (table_id, table_number, is_reserved) values (2, 2, false);
+insert into tables (table_id, table_number, is_reserved) values (3, 3, false);
+insert into tables (table_id, table_number, is_reserved) values (4, 4, false);
+insert into tables (table_id, table_number, is_reserved) values (5, 5, false);
+insert into tables (table_id, table_number, is_reserved) values (6, 6, false);
+insert into tables (table_id, table_number, is_reserved) values (7, 7, false);
+insert into tables (table_id, table_number, is_reserved) values (8, 8, false);
+insert into tables (table_id, table_number, is_reserved) values (9, 9, false);
+insert into tables (table_id, table_number, is_reserved) values (10, 10, false);
 
 CREATE TABLE vip_rooms (
     room_id INT PRIMARY KEY,
+	reserved_by text,
     room_number INT,
-    is_reserved BOOLEAN,
-    restaurant_id INT REFERENCES restaurant(restaurant_id)
+    is_reserved BOOLEAN
 );
 
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (1, 1, true, 1);
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (2, 2, false, 2);
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (3, 3, false, 3);
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (4, 4, true, 4);
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (5, 5, true, 5);
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (6, 6, false, 6);
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (7, 7, false, 7);
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (8, 8, true, 8);
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (9, 9, true, 9);
-insert into vip_rooms (room_id, room_number, is_reserved, restaurant_id) values (10, 10, false, 10);
+insert into vip_rooms (room_id, room_number, is_reserved) values (1, 1, false);
+insert into vip_rooms (room_id, room_number, is_reserved) values (2, 2, false);
+insert into vip_rooms (room_id, room_number, is_reserved) values (3, 3, false);
+insert into vip_rooms (room_id, room_number, is_reserved) values (4, 4, false);
+insert into vip_rooms (room_id, room_number, is_reserved) values (5, 5, false);
+insert into vip_rooms (room_id, room_number, is_reserved) values (6, 6, false);
+insert into vip_rooms (room_id, room_number, is_reserved) values (7, 7, false);
+insert into vip_rooms (room_id, room_number, is_reserved) values (8, 8, false);
+insert into vip_rooms (room_id, room_number, is_reserved) values (9, 9, false);
+insert into vip_rooms (room_id, room_number, is_reserved) values (10, 10, false);
 
-SELECT * FROM tables;
+create table users(
+	id serial primary key,
+	user_id BIGINT,
+	username text
+);
+
+CREATE TABLE IF NOT EXISTS reservations (
+    id SERIAL PRIMARY KEY,
+    user_id bigint,
+    table_number bigint,
+    vip_room_number bigint,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (table_number) REFERENCES tables (table_id),
+    FOREIGN KEY (vip_room_number) REFERENCES vip_rooms (room_id)
+);
+
+
+drop table reservations
+drop table vip_rooms
+drop table tables
+drop table users
+SELECT * FROM users;
 SELECT * FROM vip_rooms;
+SELECT * FROM tables;
+select * from reservations
+
--- sultan.py
+++ sultan.py
@@ -4,7 +4,7 @@ from telebot import types
  
 conn = psycopg2.connect( 
     host = "localhost", 
-    database = "postgres", 
+    database = "fornurs", 
     user = "postgres",  
     password  = "123456") 
  
@@ -12,75 +12,137 @@ cursor = conn.cursor()
 
 bot = telebot.TeleBot("6933769115:AAEizT969-xkjsupy9D3mLCVUG3CujMjgak")
 
-def is_table_reserved(table_number): 
-    cursor.execute("SELECT is_reserved FROM tables WHERE table_number = %s", (table_number,)) 
-    result = cursor.fetchone() 
-    if result: 
-        return result[0] 
-    return False 
-
-
-def is_vip_room_reserved(room_number): 
-    cursor.execute("SELECT is_reserved FROM vip_rooms WHERE room_number= %s", (room_number,)) 
-    result = cursor.fetchone() 
-    if result: 
-        return result[0] 
-    return False 
+admin_rooms = [6320258945, 5138539806]
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет, <b>{message.from_user.first_name}! </b>'
+    mess = f'Здравствуйте, <b>{message.from_user.first_name}! </b>'
     bot.send_message(message.chat.id, mess, parse_mode='html')
-
-@bot.message_handler(commands=['s'])
-def welcome(message):
     chat_id = message.chat.id
     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = telebot.types.KeyboardButton(text="Забронировать стол и проверить его доступность")
-    button2 = telebot.types.KeyboardButton(text="Забронировать VIP Кабинку")
-    button3 = telebot.types.KeyboardButton(text="Отменить бронирование стола")
-    button4 = telebot.types.KeyboardButton(text="Отменить бронирование VIP Кабинки")
+    button1 = telebot.types.KeyboardButton(text="Войти в систему как админ")
+    button2 = telebot.types.KeyboardButton(text="Войти как обычный пользователь")
     keyboard.add(button1, button2)
-    keyboard.add(button3, button4)
     bot.send_message(chat_id, 'Добро пожаловать в бота', reply_markup=keyboard)
-
-@bot.message_handler(content_types=['text'])
+    
+    
+@bot.message_handler()
 def func(message):
-    if(message.text == "Забронировать стол и проверить его доступность"):
-        bot.send_message(message.chat.id, text="Введите номер стола,который хотите забронировать:")
-        bot.register_next_step_handler(message, reserve_table)
+    chat_id = message.chat.id
+    #Производим вход
+    if message.text == 'Войти в систему как админ':
+    #
 
-    elif(message.text == "Забронировать VIP Кабинку"):
-        bot.send_message(message.chat.id, text = 'Введите желаемую кабинку:')
+        #Добавляем кнопки при входе , как админ
+        user_id = message.from_user.id
+        if user_id in admin_rooms:
+            bot.send_message(message.chat.id, "Вы вошли в систему как администратор.")
+            keyboard =types.ReplyKeyboardMarkup(resize_keyboard=True)
+            button1 = types.KeyboardButton(text="Забронированные столы")
+            button2 = types.KeyboardButton(text="Свободные столы")
+            button3 = types.KeyboardButton(text="Свободные vip-кабинки")
+            button4 = types.KeyboardButton(text="Занятые vip-кабинки")
+            button5 = types.KeyboardButton(text="Отменить бронирование стола")
+            button6 = types.KeyboardButton(text="Отменить бронирование vip-кабинки")
+            keyboard.add(button1, button2)
+            keyboard.add(button3, button4)
+            keyboard.add(button5, button6)
+            bot.send_message(chat_id, 'Выберите действие', reply_markup=keyboard)
+        else:
+            bot.send_message(message.chat.id, "Ваш ID не является администраторским.")
+        #
+
+            
+    #Выбираем ,кем мы являемся
+    elif message.text == 'Войти как обычный пользователь':
+        bot.send_message(message.chat.id,'Введите ваше имя:')
+        bot.register_next_step_handler(message , insert_name)
+    #
+
+
+    #Функция для бронирования стола
+    elif message.text == 'Забронировать стол' :
+        bot.send_message(message.chat.id, text="Введите номер стола,который хотите забронировать от 1 до 10:")
+        bot.register_next_step_handler(message, reserve_table )
+
+    #
+
+
+    #функция для бронирования вип кабинки
+    elif message.text == "Забронировать vip-кабинку" :
+        bot.send_message(message.chat.id, text='Введите желаемую кабинку от 1 до 10:')
         bot.register_next_step_handler(message, reserve_vip_room)
-    elif(message.text == "Отменить бронирование стола"):
-        bot.send_message(message.chat.id, text = " Введите стол , на котором хотите отменить бронь")
-        bot.register_next_step_handler(message, cancel_table_reservation)
-    else:
-        (message.text == "Отменить бронирование VIP Кабинки")
-        bot.send_message(message.chat.id, text ="Введите номер кабинки , у которой хотите отменить бронирование")
-        bot.register_next_step_handler(message, cancel_vip_room_reservation)
+    #
+
+        #Проверяем какие столы забронированны
+    elif message.text == 'Забронированные столы' and message.from_user.id in admin_rooms :
+        cursor.execute("SELECT table_number FROM tables WHERE is_reserved = TRUE")
+        reserved_tables = cursor.fetchall()
+        if reserved_tables:
+            reserved_tables_str = ", ".join(str(table[0]) for table in reserved_tables)
+            message_text = f"Занятые столы: {reserved_tables_str}"
+        else:
+            message_text = "Все столы свободны."
+        bot.send_message(message.chat.id, message_text)
+        #
 
 
-def reserve_table(message): 
-    if is_table_reserved(message.text): 
-        bot.send_message(message.chat.id , text ="Стол уже занят.") 
-    else: 
-        cursor.execute("UPDATE tables SET is_reserved = TRUE WHERE table_number = %s", (message.text,)) 
-        conn.commit() 
-        bot.send_message(message.chat.id , text ="Стол забронирован.") 
+        #Проверяем свободные столы
+    elif message.text == 'Свободные столы' and message.from_user.id in admin_rooms :
+        cursor.execute("SELECT table_number FROM tables WHERE is_reserved = FALSE")
+        reserved_tables = cursor.fetchall()
+        if reserved_tables:
+            reserved_tables_str = ", ".join(str(table[0]) for table in reserved_tables)
+            message_text = f"Свободные столы: {reserved_tables_str}"
+        else:
+            message_text = "Все столы заняты."
+        bot.send_message(message.chat.id, message_text)
+        #
 
 
-def reserve_vip_room(message): 
-    if is_vip_room_reserved(message.text): 
-        bot.send_message(message.chat.id , text ="VIP Кабинка уже занята.") 
-    else: 
-        cursor.execute("UPDATE vip_rooms SET is_reserved = TRUE WHERE room_number = %s", (message.text)) 
-        conn.commit() 
-        bot.send_message(message.chat.id , text ="VIP Кабинка забронирована.") 
+        #Функция для просмотра свободных vip-кабинок
+    elif message.text == 'Свободные vip-кабинки' and message.from_user.id in admin_rooms :
+        cursor.execute("SELECT room_number FROM vip_rooms WHERE is_reserved = FALSE")
+        reserved_tables = cursor.fetchall()
+        if reserved_tables:
+            reserved_tables_str = ", ".join(str(table[0]) for table in reserved_tables)
+            message_text = f"Свободные кабинки: {reserved_tables_str}"
+        else:
+            message_text = "Все кабинки заняты."
+        bot.send_message(message.chat.id, message_text)
+        #
 
 
+    #функция для просмотра занятых вип кабинок
+    elif message.text == 'Занятые vip-кабинки' and message.from_user.id in admin_rooms :
+        cursor.execute("SELECT room_number FROM vip_rooms WHERE is_reserved = TRUE")
+        reserved_tables = cursor.fetchall()
+        if reserved_tables:
+            reserved_tables_str = ", ".join(str(table[0]) for table in reserved_tables)
+            message_text = f"Занятые vip-кабинки: {reserved_tables_str}"
+        else:
+            message_text = "Все кабинки свободны."
+        bot.send_message(message.chat.id, message_text)
+    # 
+
+    #функция для отмены бронирования стола
+    elif message.text == 'Отменить бронирование стола' and message.from_user.id in admin_rooms :
+        bot.send_message(message.chat.id, 'Введите номер стола')
+        bot.register_next_step_handler(message,cancel_table_reservation)
+    #
+
+
+    #Функция для отмены бронирования вип кабинки
+    elif message.text == 'Отменить бронирование vip-кабинки' and message.from_user.id in admin_rooms :
+        bot.send_message(message.chat.id, 'Введите номер кабинки')
+        bot.register_next_step_handler(message,cancel_vip_room_reservation)
+    #
+
+
+
+
+#Дополнительные функции
 def cancel_table_reservation(message): 
     if is_table_reserved(message.text): 
         cursor.execute("UPDATE tables SET is_reserved = FALSE WHERE table_number = %s", (message.text)) 
@@ -89,7 +151,6 @@ def cancel_table_reservation(message):
     else:
         bot.send_message(message.chat.id , text ="Стол не забронирован. ")
 
-
 def cancel_vip_room_reservation(message): 
     if is_vip_room_reserved(message.text): 
         cursor.execute("UPDATE vip_rooms SET is_reserved = FALSE WHERE room_number = %s", (message.text)) 
@@ -97,6 +158,88 @@ def cancel_vip_room_reservation(message):
         bot.send_message(message.chat.id , text ="Бронирование VIP кабинки отменено.") 
     else:
         bot.send_message(message.chat.id , text ="VIP кабинка не забронирована")
+
+def is_table_reserved(table_number): 
+    cursor.execute("SELECT is_reserved FROM tables WHERE table_number = %s", (table_number,)) 
+    result = cursor.fetchone() 
+    if result: 
+        return result[0] 
+    return False 
+
+def is_vip_room_reserved(room_number): 
+    cursor.execute("SELECT is_reserved FROM vip_rooms WHERE room_number= %s", (room_number,)) 
+    result = cursor.fetchone() 
+    if result: 
+        return result[0] 
+    return False 
+
+def insert_name(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    username = message.text
     
+    # Проверяем, существует ли пользователь уже в базе данных
+    cursor.execute("SELECT username FROM users WHERE user_id = %s", (user_id,))
+    existing_username = cursor.fetchone()
+    
+    if existing_username:
+        # Обновляем имя пользователя в базе данных
+        update_query = 'UPDATE users SET username = %s WHERE user_id = %s'
+        cursor.execute(update_query, (username, user_id))
+    else:
+        # Вставляем новое имя пользователя в базу данных
+        insert_query = 'INSERT INTO users (user_id, username) VALUES (%s, %s)'
+        cursor.execute(insert_query, (user_id, username,))
+    
+    conn.commit()
+    bot.send_message(message.chat.id, "Имя успешно добавлено в базу данных.")
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton(text="Забронировать стол")
+    button2 = types.KeyboardButton(text="Забронировать vip-кабинку")
+    keyboard.add(button1, button2)
+    bot.send_message(chat_id, 'Пожалуйста выберите следующее действие:', reply_markup=keyboard)
+
+def reserve_table(message):
+    table_number = message.text
+    if not is_valid_table_number(table_number):
+        bot.send_message(message.chat.id, text="Введите корректный номер стола от 1 до 10.")
+    elif is_table_reserved(table_number):
+        bot.send_message(message.chat.id, text="Стол уже занят.")
+    else:
+        username = get_username(message.from_user.id)
+        if username:
+            cursor.execute("UPDATE tables SET is_reserved = TRUE, reserved_by = %s WHERE table_number = %s", (username, message.text))
+            conn.commit()
+            bot.send_message(message.chat.id, text=f"Стол {message.text} забронирован для пользователя {username}.")
+        else:
+            bot.send_message(message.chat.id, text="Что-то пошло не так. Пожалуйста, попробуйте еще раз.")
+
+def get_username(user_id):
+    cursor.execute("SELECT username FROM users WHERE user_id = %s", (user_id,))
+    result = cursor.fetchone()
+    return result[0] if result else None
+
+def reserve_vip_room(message):
+    room_number = message.text
+    if not is_valid_vip_room_number(room_number):
+        bot.send_message(message.chat.id, text="Введите корректный номер VIP-кабинки от 1 до 10.")
+    elif is_vip_room_reserved(room_number):
+        bot.send_message(message.chat.id, text="VIP-кабинка уже занята.")
+    else:
+        username = get_username(message.from_user.id)
+        if username:
+            cursor.execute("UPDATE vip_rooms SET is_reserved = TRUE, reserved_by = %s WHERE room_number = %s", (username, message.text))
+            conn.commit()
+            bot.send_message(message.chat.id, text=f"Кабинка {message.text} забронирована для пользователя {username}.")
+        else:
+            bot.send_message(message.chat.id, text="Что-то пошло не так. Пожалуйста, попробуйте еще раз.")
+
+def is_valid_table_number(table_number):
+    return 1 <= int(table_number) <= 10
+
+def is_valid_vip_room_number(room_number):
+    return 1 <= int(room_number) <= 10
+    #
 
 bot.infinity_polling()
+
